606c0479f8f05035e1c7fd3a944dc7aec77dd621
Benjamín Muľár <83555059+Noiyy@users.noreply.github.com>
2024-08-11 13:08:34 +0200

    Add port option to DB connection

--- data/database.js
+++ data/database.js
@@ -5,6 +5,7 @@ const pool = mysql.createPool({
     database: process.env.DB,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
+    port: process.env.DB_PORT || 3306
 });
 
 module.exports = pool;
